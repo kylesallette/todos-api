b6bb02c8da8c886ac1338a88dcc20e54678215c2
kylesallette <kylesallette@gmail.com>
2018-04-17 14:28:56 -0600

    API pratice

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :todos do
   resources :items
   end
+    post 'signup', to: 'users#create'
+    post 'auth/login', to: 'authentication#authenticate'
 end
